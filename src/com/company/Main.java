44f940f286d1f3a263e4167ebcee15b6aab7cf6d
JulzV <volkova.julija@inbox.lv>
2021-06-20 20:25:56 +0300

    Add multiplication and division

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,25 +3,32 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-// Number declaration
-        int firstNumber;
-        int secondNumber;
-        int answer;
-// Number initialised
-        firstNumber = 5;
-        secondNumber = 7;
+//  Number declaration
+        float firstNumber;
+        float secondNumber;
+        float answer;
+
+//  Number initialised
+        firstNumber = 10;
+        secondNumber = 3;
         answer = 0;
 
-// Mathematical calculator
-        String operator = "-";
+//  Defining operator
+        String operator = "/";
+
+//  Mathematical calculations
 
         if (operator.equals("+")) {
             answer = firstNumber + secondNumber;
         } else if (operator.equals("-")){
             answer = firstNumber - secondNumber;
-        } // Assignment
+        } else if (operator.equals("*")){
+            answer = firstNumber * secondNumber;
+        } else if (operator.equals("/")) {
+            answer = firstNumber / secondNumber;
+        }
 
-//  do theelse if statements for the multiplication and division
+//  Print answer
         System.out.println(answer);
 
     }
